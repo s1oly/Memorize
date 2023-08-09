5e3fd8e0231c14551d3c33462baff5ecf49ea9ed
s1oly <90860963+s1oly@users.noreply.github.com>
2023-08-08 20:33:39 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by S1OLY on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
