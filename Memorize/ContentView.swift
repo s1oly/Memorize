07d1a5e7c36430ab56506b35148e9ba9d8c176b0
s1oly <90860963+s1oly@users.noreply.github.com>
2023-08-14 14:53:58 -0400

    Added button to add/subtract amount of cards shown, changed up UI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,47 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["✈️","🚘","🛬","🗼","🚙","🛻"]
+    @State var emojiCount = 3
     var body: some View{
-        HStack{
-            CardView(FacedUp: true)
-            CardView(FacedUp: true)
-            CardView(FacedUp: true)
-            CardView(FacedUp: true)
+        VStack{
+            HStack{
+                ForEach(emojis[0..<emojiCount],id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
         }
         .padding(.horizontal)
         .foregroundColor(Color.red)
-        
-    
     }
-        
-}
-
-
-struct CardView: View{
-    var FacedUp: Bool
-    
-    var body: some View{
-        HStack{
-            ZStack{
-                if FacedUp{
-                    RoundedRectangle(cornerRadius: 20)
-                    .fill()
-                    .foregroundColor(.white)
-                    RoundedRectangle(cornerRadius: 20)
-                    .stroke(lineWidth: 3)
-                    Text("✈️")
-                    .font(Font.largeTitle)
-                }
-                else{
-                    RoundedRectangle(cornerRadius: 20)
-                    .fill()
-                    .foregroundColor(.white)
-                }
-                    
+    var remove : some View{
+        Button{
+            if emojiCount > 1{
+                emojiCount -= 1
             }
+        } label:{
+            Image(systemName: "minus.circle")
+        }
+    }
+    var add : some View{
+        Button{
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        } label:{
+            Image(systemName: "plus.circle")
         }
     }
 }
+
+
+
+
+
+
+
+struct CardView: View{
+    var content: String
+    @State var FacedUp: Bool = false
+    var body: some View{
+            ZStack{
+                let shape = RoundedRectangle(cornerRadius: 20)
+                if FacedUp{
+                    shape.fill().foregroundColor(.white)
+                    shape.stroke(lineWidth: 3)
+                    Text(content).font(Font.largeTitle)
+                }
+                else{
+                    shape.fill().foregroundColor(.red)
+                }
+                    
+            }
+            .onTapGesture{
+                FacedUp = !FacedUp
+            }
+        }
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
