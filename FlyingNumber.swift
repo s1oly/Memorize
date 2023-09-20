fd109446ba8f1e7421477e4c5a4a09f8e59e9b50
s1oly <90860963+s1oly@users.noreply.github.com>
2023-09-19 23:28:30 -0400

    Animation Changes

---
+++ FlyingNumber.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingNumber.swift
+//  Memorize
+//
+//  Created by S1OLY on 9/19/23.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingNumber()
+}
