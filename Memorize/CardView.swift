475947d8d109caee64422867eb50be972a32bbd7
s1oly <90860963+s1oly@users.noreply.github.com>
2023-09-21 17:08:23 -0400

    Hopefully Final Commit for debug

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -55,7 +55,7 @@ struct CardView: View{
 
 extension Animation {
     static func spin(duration : TimeInterval) -> Animation{
-        .linear(duration: 1).repeatForever(autoreverses:false)
+        .linear(duration: 1)
     }
 }
 
